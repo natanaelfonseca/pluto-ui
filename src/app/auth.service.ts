5bcaaead16e448f4545b54f75a6f85dc0ed510d8
natanaelfonseca <natanael.vaz@gmail.com>
2024-02-09 14:53:20 -0300

    Construindo a home da aplicação

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -5,5 +5,23 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
+  fakeEmail: string = "john@fake.com.br"
+  fakePwd: string = "123456"
+
   constructor() { }
+
+  login(email: string, password: string): boolean {
+    
+    if( email == this.fakeEmail && password == this.fakePwd ){
+      return true;
+    }
+
+    return false;
+  }  
+
+  logout(){
+    localStorage.removeItem('userEmail');
+    localStorage.removeItem('userPassword');
+  }
+
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  loginForm!: FormGroup;
+  errorMessages: Array<string> = [];
+
+  constructor(private router: Router,
+    private authService: AuthService ) { }
+
+  ngOnInit() {
+
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      remember: new FormControl('', [Validators.required])
+    });
+
+    const storedEmail = localStorage.getItem('userEmail');
+    const storedPassword = localStorage.getItem('userPassword');
+
+    if (storedEmail && storedPassword) {
+      this.loginForm.patchValue({ email: storedEmail, password: storedPassword });
+      this.loginForm.get('remember')?.patchValue(true); // Check the remember box
+    }
+
+  }
+
+  login() {
+
+    if (this.loginForm.valid) {
+
+      const email = this.loginForm.value.email;
+      const password = this.loginForm.value.password;
+
+      if (this.authService.login(email, password)) {
+
+        if (this.loginForm.value.remember) {
+          localStorage.setItem('userEmail', email);
+          localStorage.setItem('userPassword', password);
+        }
+
+        this.router.navigate(['/home']);
+
+      } else {
+        this.errorMessages.push("Credenciais inválidas");
+      }
+
+    } else {
+
+      this.errorMessages = []; // Clear any previous error message
+
+      if (this.loginForm?.get('email')?.hasError('required')) {
+        this.errorMessages.push("O email é obrigatório.");
+      }
+      if (this.loginForm?.get('email')?.hasError('email')) {
+        this.errorMessages.push("Por favor, forneça um e-mail válido");
+      }
+      if (this.loginForm?.get('password')?.hasError('required')) {
+        this.errorMessages.push("O campo password é obrigatório");
+      }
+      if (this.loginForm?.get('password')?.hasError('minlength')) {
+        this.errorMessages.push("O campo password deve conter no mínimo 6 caracteres");
+      }
+
+    }
+
+  }
 
 }
